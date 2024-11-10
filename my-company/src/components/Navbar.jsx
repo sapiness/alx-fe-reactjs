5cbdedc1da14e3636c08a59968e3613935c1de5a
Surprise <sapiness695@gmail.com>
2024-11-11 00:21:20 +0200

    Simple Company Website with React with inline style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
    return (
 <nav className="Navbar">
    
-    <div className="links" style={{ backgroundColor: 'grey', display: 'Flex', justifyContent: 'Center' }}>
-    <Link to="/">Home</Link>
-    <Link to="/About">About</Link>
-    <Link to="/Services">Services</Link>
+    <div className="links" style={{ backgroundColor: 'violet', display: 'Flex', justifyContent: 'Center' }}>
+    <Link to="/" style={{paddingRight: '10px'}}>Home</Link>
+    <Link to="/About" style={{paddingRight: '10px'}}>About</Link>
+    <Link to="/Services"style={{paddingRight: '10px'}}>Services</Link>
     <Link to="/Contact">Contact</Link>
     </div>
 </nav>
