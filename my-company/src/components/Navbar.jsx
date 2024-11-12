cc44f1dfffa45466b690ae77eda1f523a6e4a090
Surprise <sapiness695@gmail.com>
2024-11-12 20:19:31 +0200

    3. Build a Simple Company Website with React

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="links" style={{ backgroundColor: 'violet', display: 'Flex', justifyContent: 'Center' }}>
     <Link to="/" style={{paddingRight: '10px'}}>Home</Link>
     <Link to="/About" style={{paddingRight: '10px'}}>About</Link>
-    <Link to="/Services"style={{paddingRight: '10px'}}>Services</Link>
+    <Link to="/Services" style={{paddingRight: '10px'}}>Services</Link>
     <Link to="/Contact">Contact</Link>
     </div>
 </nav>
