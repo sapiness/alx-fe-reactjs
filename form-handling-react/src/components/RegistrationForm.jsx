c56aa9f7fef8fc34e339e0d8470999a9d8a98ed7
Surprise <sapiness695@gmail.com>
2024-11-24 03:39:25 +0200

    Form Handling in React with Controlled Components and Formik

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,35 +1,30 @@
-// import React from "react";
+
 import { useState } from 'react';
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
-// import * as Yup from 'yup';
 
 
 
 
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({ username: '', email: '' , password: ''});
-    
-    const handleChange = (e) => {
-        const {  name, value } = e.target;
-        setFormData(prevState => ({ ...prevState, [name]: value }));
+   
+    const [username, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    };
-    
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formData);
+       
     };
     return (
         <div>
            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username"  required onChange={handleChange}/>
+                <input type="text" id="username" value={username}  required onChange={(e) => setName(e.target.value)}/>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email"  required onChange={handleChange}/>
+                <input type="email" id="email" value={email}  required onChange={(e) => setEmail(e.target.value)}/>
 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password"  required onChange={handleChange}/>
+                <input type="password" id="password" value={password} required onChange={(e) => setPassword(e.target.value)}/>
                 <button>Submit</button>
 
             </form>
