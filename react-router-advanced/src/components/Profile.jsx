b87bfee47e4787b36aa74015b59ef4ddc5ce1ee5
Surprise <sapiness695@gmail.com>
2024-11-25 00:14:49 +0200

    added  Routes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Switch, Link, useParams } from 'react-router-dom';
 import ProfileDetails from './ProfileDetails';
 import ProfileSettings from './ProfileSettings';
 const Profile = () => {
   return (
     <Router>
+        <Routes>
       <div>
         <h1>Profile</h1>
         <ul>
@@ -12,11 +13,16 @@ const Profile = () => {
           <li><Link to="/profile/settings">Settings</Link></li>
         </ul>
         <Switch>
+        <Route>
           <Route path="/profile/details" component={ProfileDetails} />
           <Route path="/profile/settings" component={ProfileSettings} />
+          </Route>
         </Switch>
+       
       </div>
+      </Routes>
     </Router>
   );
 };
+
 export default Profile;
