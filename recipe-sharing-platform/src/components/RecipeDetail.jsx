e9a0bae44ff4199f18adb036420c89acea23c115
Surprise <sapiness695@gmail.com>
2024-12-08 23:32:28 +0200

    recipe details

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,45 +1,25 @@
-import React, { useState, useEffect } from 'react';
-function HomePage() {
-  const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const loadRecipes = async () => {
-      try {
-        const response = await fetch('./alx-fe-reactjs/recipe-sharing-platform/src/data.json', {headers : {'Content-Type': 'application/json', 'Accept': 'application/json'}});
-        const data = await response.json();
-        setRecipes(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    loadRecipes();
-  }, []);
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  if (error) {
-    return <p>Error: {error}</p>;
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { useParams } from 'react-router-dom';
+function RecipeDetail({ recipes }) {
+  const { id } = useParams();
+  const recipe = recipes.find((recipe) => (recipe) === parseInt(id));
+  if (!recipe) {
+    return <p>Recipe not found!</p>;
   }
   return (
     <div>
-      <h1>Recipe Sharing Platform</h1>
-      <card className='rounded-lg shadow-lg hover:bg-blue-700 text-black sm:w-400 md:w-768 grid'>
-      
+      <h1>{recipe.title}</h1>
+      <img src={recipe.image} alt={recipe.title} />
+      <h2>Ingredients:</h2>
       <ul>
-        {recipes.map((recipe) => (
-          <li className='md:grid-cols-1' key={(recipe.id)}>
-            <h2 className='md:grid-cols-2' >{recipe.title}</h2>
-            <p className='md:grid-cols-3' >{recipe.summary}</p>
-           
-          </li>
+        {recipe.ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
         ))}
       </ul>
-      </card>
+      <h2>Instructions:</h2>
+      <p>{recipe.instructions}</p>
     </div>
   );
 }
-export default HomePage;
-      
+export default RecipeDetail;
