af9285323aa1d13937d082e8f6e02bab8a2654f0
Surprise <sapiness695@gmail.com>
2024-12-08 23:36:50 +0200

    useeefect

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,9 +1,31 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-function RecipeDetail({ recipes }) {
+function RecipeDetail() {
   const { id } = useParams();
-  const recipe = recipes.find((recipe) => (recipe) === parseInt(id));
+  const [recipe, setRecipe] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const loadRecipe = async () => {
+      try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        const recipeData = data.find((recipe) => (recipe) === parseInt(id));
+        setRecipe(recipeData);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadRecipe();
+  }, [id]);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
   if (!recipe) {
     return <p>Recipe not found!</p>;
   }
