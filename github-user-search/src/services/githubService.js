4805762e817baabaf5b01ffe72ca3123ec4cb055
Surprise <sapiness695@gmail.com>
2024-12-15 14:07:38 +0200

    link

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -10,7 +10,7 @@
 import axios from 'axios';
 const githubService = {
   fetchAdvancedUserData: async (query, location, minRepos) => {
-    const url = `('https://api.github.com'):>=${minRepos}+${query}`;
+    const url = `("https://api.github.com/search/users?q"):>=${minRepos}+${query}`;
     const response = await axios.get(url);
     return response.data;
   },
